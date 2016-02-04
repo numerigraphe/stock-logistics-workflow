65c9de450f34d83abbe1161a688dd79a55edc0f5
Lionel Sausin <ls@numerigraphe.com>
2016-02-04 14:49:28 +0100

    Revert "[IMP] Restrict blocking/unblocking to a specific group"
    This implementation was faulty. A better implementation is already merged in prod.
    This reverts commit 3897d4024d1daa05dd6a93ee27a6b99086f6194f.

--- stock_lock_lot/models/stock_production_lot.py
+++ stock_lock_lot/models/stock_production_lot.py
@@ -38,8 +38,7 @@ class StockProductionLot(models.Model):
         return self._get_product_locked(self.product_id)
 
     locked = fields.Boolean(string='Blocked', default='_get_locked_value',
-                            readonly=True,
-                            groups='stock_lock_lot.group_lock_lot')
+                            readonly=True)
 
     @api.one
     @api.onchange('product_id')

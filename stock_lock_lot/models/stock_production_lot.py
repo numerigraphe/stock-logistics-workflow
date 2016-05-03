b241476269c0f925f51511c0b0379c6ce14d6409
Lionel Sausin <ls@numerigraphe.com>
2016-05-03 12:08:11 +0200

    Missing import caused by wrong merge

--- stock_lock_lot/models/stock_production_lot.py
+++ stock_lock_lot/models/stock_production_lot.py
@@ -3,7 +3,7 @@
 # © 2015 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions, _
 
 
 class StockProductionLot(models.Model):
